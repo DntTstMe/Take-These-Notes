0816c4a4d045a8ceadc23e97184c522b993d5c18
Alex Tester <lex.tester93@gmail.com>
2023-08-17 13:17:56 -0400

    fixed writeNotesFile function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,6 +14,14 @@ async function readNotesFile() {
         throw new Error("Failed to read notes.");
     }
 }
+async function writeNotesFile(data){
+ try {
+    const notes = await fs.writeFile(dbFilePath, JSON.stringify(data));
+    return JSON.parse(notes);
+    } catch (err) {
+    throw new Error("Failed to read notes.");
+    }
+ }
 // Route to get all notes
 router.get('/notes', async (req, res) => {
     try {
@@ -28,8 +36,10 @@ router.get('/notes', async (req, res) => {
 router.post('/notes', async (req, res) => {
     try {
         const jsonNotes = await readNotesFile();
+        console.log(jsonNotes);
         const newNote = { ...req.body, id: Date.now().toString() };
         jsonNotes.push(newNote);
+        console.log(jsonNotes);
         await writeNotesFile(jsonNotes);
         res.json(newNote);
     } catch (err) {
